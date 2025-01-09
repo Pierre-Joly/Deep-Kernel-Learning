d620dd97a25a1643d4fe254e37e0c1f2504b8437
pjoly <pierre.joly@ecl21.ec-lyon.fr>
2025-01-09 11:08:48 +0100

    fonction associate to GP

--- GP-Dataset/create_data.py
+++ GP-Dataset/create_data.py
@@ -1,37 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Parameters
-N = 100
-min = 0
-max = 10
+# Square Exponential Kernel
+def square_exponential_kernel(X, length_scale=1.0, sigma_f=1.0, noise=1e-6):
+    sqdist = np.sum(X**2, axis=1).reshape(-1, 1) + np.sum(X**2, axis=1) - 2 * np.dot(X, X.T)
+    K = sigma_f**2 * np.exp(-0.5 * sqdist / length_scale**2)
+    K += noise * np.eye(X.shape[0])
+    return K
 
-# Kernel
-def square_exponential_kernel(x1, x2, length_scale=1.0, sigma_f=1.0):
-    sqdist = np.sum(x1**2, axis=1).reshape(-1, 1) + np.sum(x2**2, axis=1) - 2 * np.dot(x1, x2.T)
-    return sigma_f**2 * np.exp(-0.5 * sqdist / length_scale**2)
-
-#  Uniform Grid
-X = np.linspace(min, max, N)
-X = X[:, None]
-
-# Covariance Matrix
-length_scale = 1.0
-sigma_f = 1.0
-K = square_exponential_kernel(X, X, length_scale, sigma_f)
-
-# Increase numerical stability
-K += 1e-6 * np.eye(N)
-
-# Mean function
-mean = np.zeros(N)
-
-# Simulate GP
-GP = np.random.multivariate_normal(mean, K, size=3)
+# Gaussian Process
+def gaussian_process(X, mean, kernel, size=1):
+    M = mean(X)
+    K = kernel(X)
+    return np.random.multivariate_normal(M, K, size=size)
 
 if __name__ == '__main__':
+    # Parameters
+    N = 100
+    x_min, x_max = 0, 10
+    number_of_trajectories = 3
+
+    #  Uniform Grid
+    X = np.linspace(x_min, x_max, N)[:, None]
+
+    # Covariance Matrix
+    length_scale = 1.0
+    sigma_f = 1.0
+    kernel_func = lambda X: square_exponential_kernel(X, length_scale, sigma_f, noise=1e-6)
+    mean_func = lambda x: np.zeros(x.shape[0])
+
+    # Simulate GP
+    GP = gaussian_process(X, mean_func, kernel_func, size=number_of_trajectories)
+
     plt.figure()
-    for i in range(3):
+    for i in range(number_of_trajectories):
         plt.plot(X, GP[i], label=f'Sample {i+1}')
     plt.title('Simulated Gaussian Process Samples')
     plt.xlabel('X')
